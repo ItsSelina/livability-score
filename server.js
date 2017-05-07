9eba309e691f9ab31850fc96155e15b830c70391
Selina Li <li.selina123@gmail.com>
2017-05-07 13:14:13 -0400

    Initial

---
+++ server.js
@@ -0,0 +1,26 @@
+var express = require('express'),
+  app = express(),
+  port = process.env.PORT || 3000,
+  mongoose = require('mongoose'),
+  Task = require('./api/models/todoListModel'),
+  bodyParser = require('body-parser');
+  
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost/Tododb'); 
+
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.use(function(req, res) {
+  res.status(404).send({url: req.originalUrl + ' not found'})
+});
+
+var routes = require('./api/routes/todoListRoutes');
+routes(app);
+
+
+app.listen(port);
+
+
+console.log('todo list RESTful API server started on: ' + port);
