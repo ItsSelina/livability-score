deafd1bd8d6345d2f864936a42798e137f7a55db
Selina Li <li.selina123@gmail.com>
2017-05-08 13:44:31 -0400

    Return placeholder score from GET request

--- server.js
+++ server.js
@@ -2,25 +2,21 @@ var express = require('express'),
   app = express(),
   port = process.env.PORT || 3000,
   mongoose = require('mongoose'),
-  Task = require('./api/models/todoListModel'),
+  livabilityScore = require('./api/models/livabilityScoreModel'),
   bodyParser = require('body-parser');
   
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost/Tododb'); 
+mongoose.connect('mongodb://localhost/db'); 
 
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-app.use(function(req, res) {
-  res.status(404).send({url: req.originalUrl + ' not found'})
-});
-
-var routes = require('./api/routes/todoListRoutes');
+var routes = require('./api/routes/livabilityScoreRoutes');
 routes(app);
 
 
 app.listen(port);
 
 
-console.log('todo list RESTful API server started on: ' + port);
+console.log('livabilityScore RESTful API server started on: ' + port);
