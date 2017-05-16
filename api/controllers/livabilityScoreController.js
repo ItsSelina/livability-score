5cc9176c76262b777b4390359b8a3b9e9058ee11
Selina Li <li.selina123@gmail.com>
2017-05-16 19:36:53 -0400

    Use promises instead of request-promises

--- api/controllers/livabilityScoreController.js
+++ api/controllers/livabilityScoreController.js
@@ -5,8 +5,8 @@ const maxRecreation = 300;
 
 var mongoose = require('mongoose');
 var request = require('request');
-var rp = require('request-promise');
 
+var livabilityScore;
 var walkScore;
 var transitScore;
 var restaurantScore;
@@ -46,52 +46,80 @@ exports.getLivabilityScore = function(req, res) {
     url: `http://api.walkscore.com/score?format=json&lat=${req.query.lat}&lon=${req.query.lng}&transit=1&bike=1&wsapikey=${process.env.WALKSCORE_KEY}`
   };
 
-  rp(walkScoreOptions)
-    .then(function(response) {
-      var walkScoreResponse = JSON.parse(response);
-      walkScore = walkScoreResponse.walkscore;
-      transitScore = walkScoreResponse.transit.score;
+  var fetchWalkScore = new Promise(
+    function(resolve, reject) {
+      request(walkScoreOptions, function(error, response, body) {
+        if (error != null) reject(error);
+        var walkScoreResponse = JSON.parse(body);
 
-      return rp(restaurantOptions);
-    })
-    .then(function(response) {
-      var restaurantResponse = JSON.parse(response);
-      restaurantScore = restaurantResponse.total/maxRestaurants * 100;
+        resolve([walkScoreResponse.walkscore, walkScoreResponse.transit.score]);
+      });
+    });
 
-      if (restaurantScore > 100) {
-        restaurantScore = 100;
-      }
+  var fetchRestaurantScore = new Promise(
+    function(resolve, reject) {
+      request(restaurantOptions, function(error, response, body) {
+        if (error != null) reject(error);
+        var restaurantResponse = JSON.parse(body);
+        restaurantScore = restaurantResponse.total/maxRestaurants * 100;
 
-      return rp(gymOptions);
-    })
-    .then(function(response) {
-      var gymResponse = JSON.parse(response);
-      recreationScore = 0;
-      recreationScore = recreationScore + gymResponse.total;
+        if (restaurantScore > 100) {
+          restaurantScore = 100;
+        }
 
-      return rp(parkOptions);
-    })
-    .then(function(response) {
-      var parkResponse = JSON.parse(response);
-      recreationScore = recreationScore + parkResponse.total;
+        resolve(restaurantScore);
+      });
+    });
 
-      return rp(mallOptions);
-    })
-    .then(function(response) {
-      var mallResponse = JSON.parse(response);
-      recreationScore = recreationScore + mallResponse.total;
-      //
-      recreationScore = recreationScore * 100/maxRecreation;
+  var fetchGymScore = new Promise(
+    function(resolve, reject) {
+      request(gymOptions, function(error, response, body) {
+        if (error != null) reject(error);
+        var gymResponse = JSON.parse(body);
+
+        resolve(gymResponse.total);
+      });
+    });
+
+  var fetchParkScore = new Promise(
+    function(resolve, reject) {
+      request(parkOptions, function(error, response, body) {
+        if (error != null) reject(error);
+        var parkResponse = JSON.parse(body);
+        resolve(parkResponse.total);
+      });
+    });
+
+  var fetchMallScore = new Promise(
+    function(resolve, reject) {
+      request(mallOptions, function(error, response, body) {
+        if (error != null) reject(error);
+        var mallResponse = JSON.parse(body);
+        resolve(mallResponse.total);
+      });
+    });
+
+  Promise.all([fetchWalkScore, fetchRestaurantScore, fetchGymScore,
+    fetchParkScore, fetchMallScore])
+    .then(values => {
+      walkScore = values[0][0];
+      transitScore = values[0][1];
+      restaurantScore = values[1];
+      recreationScore = (values[2] + values[3] + values[4]) * 100 / maxRecreation;
+
       if (recreationScore > 100) recreationScore = 100;
 
-      var livabilityScore = walkScore/6 + transitScore/6 + restaurantScore/3 + recreationScore/3;
+      livabilityScore = walkScore/6 + transitScore/6 + restaurantScore/3 + recreationScore/3;
 
       res.json({
         "livabilityScore":livabilityScore,
         "walkScore":walkScore,
         "transitScore":transitScore,
         "restaurantScore":restaurantScore,
-        "recreationScore":recreationScore});
+        "recreationScore":recreationScore
+      });
     })
-    .catch(err => console.log);
+    .catch(function(e) {
+      console.log(e);
+    });
 };
