391da3e8c2e2425fbfbae12e45207072143f2fa5
Selina Li <li.selina123@gmail.com>
2017-05-20 15:46:32 -0400

    Remove list endpoint

--- api/routes/livabilityScoreRoutes.js
+++ api/routes/livabilityScoreRoutes.js
@@ -6,9 +6,6 @@ module.exports = function(app) {
   app.route('/score/')
     .get(controller.getLivabilityScore);
 
-  app.route('/list/')
-    .get(controller.listCrime);
-
   app.use(function(req, res) {
     res.status(404).send({error: req.originalUrl + ' not found'})
   });
